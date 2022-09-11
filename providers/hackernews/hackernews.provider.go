9cc12439a52d537fd856580a8e6f3e14c13e30b4
Tony BELLAVIA <bbellavi@e1r5p2.clusters.42paris.fr>
2022-09-11 18:07:52 +0200

    Add: HackerNews content provider

---
+++ providers/hackernews/hackernews.provider.go
@@ -0,0 +1,54 @@
+package hackernews
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"newsbot/providers"
+)
+
+const hackernews_name = "hackernews"
+
+func getStoryUrl(id int) string {
+	return fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", id)
+}
+
+func getTopstories(n int) []int {
+	url := "https://hacker-news.firebaseio.com/v0/topstories.json"
+	var topstories []int
+
+	response, err := http.Get(url)
+	if err != nil {
+		// TODO: Log in a file
+		log.Fatalf("%s: error occured while fetching top stories", hackernews_name)
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&topstories)
+	if err != nil {
+		log.Fatalf("%s: error while stories id", hackernews_name)
+	}
+	return topstories[:n]
+}
+
+func HackernewsProvider(max int) []providers.Content {
+	ids := getTopstories(max)
+	stories := make([]providers.Content, 0, max)
+	var story providers.Content
+
+	for _, id := range ids {
+		response, err := http.Get(getStoryUrl(id))
+
+		if err != nil {
+			log.Fatalf("%s: error while fetching story content", hackernews_name)
+		}
+		err = json.NewDecoder(response.Body).Decode(&story)
+		if err != nil {
+			log.Fatalf("%s: error while decoding story", hackernews_name)
+		}
+		story.Source = hackernews_name
+		stories = append(stories, story)
+	}
+	return stories
+}
