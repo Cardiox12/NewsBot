045d3c63d27fd2c1bf4e7eeca610c4ede9bec7e3
Tony BELLAVIA <bbellavi@e1r5p2.clusters.42paris.fr>
2022-09-11 18:08:16 +0200

    Add: HackernewsProvider to Provider in main.go

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"newsbot/providers"
+	"newsbot/providers/hackernews"
+)
 
+// provider -> provide interface to a list of source provider
+// content provider -> provide interface to a news website
+// content -> provide interface to a source provider content
+
+func main() {
+	provider := providers.Provider{Max: 5}
+
+	provider.RegisterContentProvider(hackernews.HackernewsProvider)
+
+	contents := provider.ProvideContents()
+	for _, content := range contents {
+		fmt.Println(content)
+	}
 }
