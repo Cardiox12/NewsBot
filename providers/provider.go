8a34e947fc6ce204688e7a5e59285f2de1aeb2e3
Tony BELLAVIA <bbellavi@e1r5p2.clusters.42paris.fr>
2022-09-11 17:43:27 +0200

    Add: content provider base

---
+++ providers/provider.go
@@ -0,0 +1,33 @@
+package providers
+
+import "fmt"
+
+type Content struct {
+	Title  string `json:"title"`
+	Url    string `json:"url"`
+	Source string `json:"source"`
+}
+
+type ContentProvider = func(max int) []Content
+
+type Provider struct {
+	Max              int
+	ContentProviders []ContentProvider
+}
+
+func (p *Provider) RegisterContentProvider(cp ContentProvider) {
+	p.ContentProviders = append(p.ContentProviders, cp)
+}
+
+func (p *Provider) ProvideContents() []Content {
+	all := make([]Content, 0)
+
+	for _, contentProvider := range p.ContentProviders {
+		all = append(all, contentProvider(p.Max)...)
+	}
+	return all
+}
+
+func (c Content) String() string {
+	return fmt.Sprintf("[%s] %s (%s)", c.Source, c.Title, c.Url)
+}
