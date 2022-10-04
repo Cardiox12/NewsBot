0b5dc6e41573e3845605a5d50777eb5998bb8c63
Tony <bbellavi@student.42.fr>
2022-10-04 23:01:56 +0200

    Fix: #3

--- providers/devnet/devnet.go
+++ providers/devnet/devnet.go
@@ -16,18 +16,27 @@ func getUrl(name string) string {
 	return fmt.Sprintf("https://%s.developpez.com/index/rss", name)
 }
 
-func devnetProvider(name string, max int, _ *database.Database) []providers.Content {
+func devnetProvider(name string, max int, db *database.Database) []providers.Content {
 	c := colly.NewCollector()
 	n := 0
 	articles := make([]providers.Content, 0)
 	url := getUrl(name)
+	stop := false
+	source := getSource(name)
 
 	c.OnXML("//item", func(e *colly.XMLElement) {
-		if n < max {
+		if n < max && !stop {
 			article := providers.Content{
 				Title: e.ChildText("title"),
 				Url: e.ChildText("link"),
-				Source: getSource(name),
+				Source: source,
+			}
+			if n == 0 {
+				if article.Exists(source, db) {
+					stop = true
+					return
+				}
+				db.Set(source, article.Hash())
 			}
 			articles = append(articles, article)
 			n++
